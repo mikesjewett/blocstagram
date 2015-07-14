b7924a94677f1e826f73f96d8a82728fc543f9bd
Mike Jewett <mikesjewett@gmail.com>
2015-07-14 07:57:29 -0400

    First commit

---
+++ Blocstagram/Blocstagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blocstagram
+//
+//  Created by Mike Jewett on 7/14/15.
+//  Copyright (c) 2015 Mike Jewett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Blocstagram/Blocstagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Blocstagram
+//
+//  Created by Mike Jewett on 7/14/15.
+//  Copyright (c) 2015 Mike Jewett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
