566032357a8ae3954a1c66c8c0db658b794b3736
Mike Jewett <mikesjewett@gmail.com>
2015-07-16 08:19:30 -0400

    Completed Building Blocstagram

--- Blocstagram/Blocstagram/ImagesTableViewController.h
+++ Blocstagram/Blocstagram/ImagesTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  ImagesTableViewController.h
 //  Blocstagram
 //
 //  Created by Mike Jewett on 7/14/15.
@@ -8,8 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ImagesTableViewController : UITableViewController
 
+@property (nonatomic, strong) NSMutableArray *images;
 
 @end
-
